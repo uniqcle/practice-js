88365c8cc1adfeaf974f82bad700eb1826b541c9
uniqcle <uniqcle@yandex.ru>
2023-04-03 14:15:27 +0700

    modified

--- js/01-show-hide-block.js
+++ js/01-show-hide-block.js
@@ -1,7 +1,14 @@
 const btn = document.querySelector('#button');
 const content = document.querySelector("#content");
 
-
 btn.addEventListener('click', function () {
+
+    content.classList.toggle('content-hidden');
+
+    if (content.classList.contains('content-hidden')) {
+        btn.textContent = "Открыть блок";
+    } else {
+        btn.textContent = "Закрыть блок";
+    }
 
 })
